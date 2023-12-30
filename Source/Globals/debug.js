94b243277dbff228bd6c38e61eab82e5e60b2ba1
H Trayford <bowman_trayford@hotmail.com>
2023-12-30 16:44:33 -0700

    Add comments to Debug.js

--- Source/Globals/debug.js
+++ Source/Globals/debug.js
@@ -2,23 +2,31 @@ import { Races, CharacterClasses } from "./characterAttributes.js"
 import InputOptionsKeys from "../Keys/inputOptionsKeys.js"
 import SceneKeys from "../Keys/sceneKeys.js"
 
-export const SkipTitleScene = true
-export const SkipCharacterCreateScene = true
+// Skip the title scene and character create scene
+export const SkipTitleScene = false
+export const SkipCharacterCreateScene = false
 
+// How many characters to create if skipping the character create scene
 export const DefaultCharacterCount = 2
+
+// Character attributes if skipping the character create scene
 export const Player1Race  = Races.Elven
 export const Player1Class = CharacterClasses.Warrior
 export const Player1Input = InputOptionsKeys.Arrows
+
 export const Player2Race = Races.Human
 export const Player2Class = CharacterClasses.Archer
 export const Player2Input = InputOptionsKeys.Gamepad1
+
 export const Player3Race = Races.Dwarven
 export const Player3Class = CharacterClasses.Magi
 export const Player3Input = InputOptionsKeys.Gamepad2
+
 export const Player4Race = Races.Human
 export const Player4Class = CharacterClasses.Cleric
 export const Player4Input = InputOptionsKeys.Gamepad3
 
+// Which level to load
 export const LevelToLoad = SceneKeys.Level1
 
 const Debug = {
