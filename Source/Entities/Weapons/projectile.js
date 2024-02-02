19e09169786f17c34b621fbed7a09a676f7251af
Vitordavi-marques <vitordavi.marques@gmail.com>
2024-02-01 22:04:06 -0300

    Projectile drop shadow

--- Source/Entities/Weapons/projectile.js
+++ Source/Entities/Weapons/projectile.js
@@ -9,12 +9,19 @@ export default class Projectile extends Phaser.GameObjects.Sprite {
 		super(scene, 0, 0, spriteSheet, frame)
 
 		this.scene = scene
+		this.depth = 10
 		this.projectileType = config.projectileType
-    this.entityType = getEntityType(config.projectileType)
+		this.entityType = getEntityType(config.projectileType)
 		this.team = config.team
 		this.damage = config.damage
 		this.range = config.range
 		this.distance = getDistanceForRange(this.range)
+		this.direction = new Phaser.Math.Vector2(0.0, 0.0)
+
+		this.dropShadow = this.scene.add.sprite(this.x, this.y, spriteSheet, frame)
+		this.dropShadow.depth = this.depth - 1
+		this.dropShadow.setTint(Phaser.Display.Color.GetColor(0, 0, 0))
+		this.dropShadow.setAlpha(0.5)
 
 		this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
 	}
@@ -22,38 +29,48 @@ export default class Projectile extends Phaser.GameObjects.Sprite {
 	update(time, delta) {
 		if (!this.scene) return
 		if (Phaser.Math.Distance.Between(this.startX, this.startY, this.x, this.y) >= this.distance) {
-			this.destroy()
+			this.shutdown()
 			return
 		}
-		
+
+		this.dropShadow.x = this.x + 4
+		this.dropShadow.y = this.y + 4
+
 		this.updateFacingDirection()
 	}
 
 	updateFacingDirection() {
-    const angle = Phaser.Math.Angle.Between(0, 0, this.body.velocity.x, this.body.velocity.y)
-    this.angle = Phaser.Math.RadToDeg(angle)
+		const angle = Phaser.Math.Angle.Between(0, 0, this.body.velocity.x, this.body.velocity.y)
+		this.angle = Phaser.Math.RadToDeg(angle)
+		this.dropShadow.angle = this.angle
 	}
 
 	didCollideWith(otherEntity) {
 		if (otherEntity.entityType === EntityTypes.Tile) {
-			this.destroy()
+			this.shutdown()
 			return
 		}
 
 		if (this.team != otherEntity.team) {
 			console.log('Dealt ', this.damage, ' damage')
 			otherEntity.takeDamage(this.damage)
-			this.destroy()
+			this.shutdown()
 		}
 	}
 
 	shoot(x, y, direction, speed) {
+		this.direction = direction
 		this.x = x + direction.x * 16
 		this.y = y + direction.y * 16
 		this.startX = this.x
 		this.startY = this.y
 		this.body.setVelocity(speed * direction.x, speed * direction.y)
 		this.body.setCollideWorldBounds(true)
+	}
+
+	shutdown() {
+		this.dropShadow.destroy()
+		this.destroy()
 	}
 }
 
